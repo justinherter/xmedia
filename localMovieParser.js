93154509bd49ba3e7db737100150a6a283ced9c2
justinherter <jherter80@gmail.com>
2019-05-28 16:39:52 -0700

    name parser

--- localMovieParser.js
+++ localMovieParser.js
@@ -7,13 +7,19 @@ const LocalStagedPath = '/data/staged';
 const FileSystem = require('./services/fileSystem').FileSystem;
 
 function run() {
+    console.log('+++++++++++++++++++++++++++ : ',process.env['STAGE']);
     let files = FileSystem.readDirectory(LocalTempPath);
     if (files.length > 0) {
         console.log(files);
         files.forEach(file => {
-            var simple = Service.simplifyFileName(item.fileName);
+            console.log(file);
+
+            var simple = Service.simplifyFileName(file);
             console.log(simple);
+            console.log(process.env['STAGE']);
+            if(!process.env['STAGE']) FileSystem.rename(`${LocalTempPath}/${file}`, `${LocalStagedPath}/${simple}`);
         });
     }
 }
+
 run();
